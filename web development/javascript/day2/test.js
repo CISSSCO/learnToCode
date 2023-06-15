0e7d53439b93ff2dbaed156ee5b02a8e594586b5
Cisco Ramon <cisssco581b@gmail.com>
2023-06-15 15:44:25 +0530

    javascript day1 and day2

---
+++ web development/javascript/day2/test.js
@@ -0,0 +1,20 @@
+a = 5;
+b = 6.2;
+x = "Cisco Ramon"
+console.log(typeof x);
+console.log(x);
+console.log(typeof a);
+console.log(typeof b);
+console.log(a + b);
+
+var m = 12;
+console.log(m);
+let n = 13;
+console.log(n);
+
+z = false
+console.log(typeof z);
+
+var p;  //declaration
+p = 5;
+console.log(typeof p);
---
+++ web development/javascript/day2/test2.js
@@ -0,0 +1,18 @@
+
+firstname = "Cisco";
+lastname = "Ramon";
+console.log(firstname + " " + lastname);
+
+my_name$123 = "Cisco ";
+console.log(my_name$123);
+a123name = "hello";
+// 123name = "hello"; not allowed starting with numbers
+console.log(a123name);
+$myage = 23;
+console.log(typeof $myage);
+
+whoAreYou = "Cisco \nRamon";
+whoAmI = 'Cisco \'Ramon';
+// who are you = "Cisco"; Not allowed
+console.log(whoAreYou);
+console.log(whoAmI);
